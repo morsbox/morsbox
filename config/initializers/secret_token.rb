5bedd3fad8a0dc1ef1d8d230e7cf26c2a1a75a46
Mik_Die <MikDiet@gmail.com>
2011-05-30 04:18:43 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Morsbox::Application.config.secret_token = '6f9abaf841fb410d2ea1981a8317c2bd3dfc49057aa2a51f7db7aa23a27bc6bc0a854f8e1af52e39e0c10e8304fc8084a732c62a661338b8a734089361296ee6'
