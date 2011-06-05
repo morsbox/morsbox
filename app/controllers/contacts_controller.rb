790dae356c6bc114c63a415e7fb0ab2b64d6848e
Mik_Die <MikDiet@gmail.com>
2011-06-05 18:25:42 +0800

    create action of contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,8 +1,17 @@
 class ContactsController < ApplicationController
   def index
     @contact_info = StaticBlock.get_block("contacts").text
+    @contact = ContactMessage.new flash[:contact]
   end
   
   def create
+    contact = ContactMessage.new params[:contact]
+    if contact.save
+      flash[:notice] = t.contacts.message_sent
+    else
+      flash[:alert] = t.contacts.message_errors + contact.errors.full_messages*", "
+      flash[:contact] = params[:contact]
+    end
+    redirect_to contacts_path
   end
 end
--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -3,8 +3,10 @@ require 'spec_helper'
 describe ContactsController do
   describe "GET index" do
     let(:contacts_static_block){mock_model(StaticBlock, :text => "Greetings!")}
+    let(:contact_message){mock_model(ContactMessage).as_new_record}
     before :each do
       StaticBlock.stub(:get_block).and_return contacts_static_block
+      ContactMessage.stub(:new).and_return contact_message
     end
     
     it "asks 'contacts' static block" do
@@ -15,6 +17,64 @@ describe ContactsController do
     it "assigns text of 'contacts' static block" do
       get :index, :locale => 'en'
       assigns[:contact_info].should == "Greetings!"
+    end
+    
+    it "assigns new contact message to @contact" do
+      get :index, :locale => 'en'
+      assigns[:contact].should == contact_message
+    end
+    
+    it "creates new contact message from flash[:contact]" do
+      ContactMessage.should_receive(:new).with "name"=>"Vasia"
+      get :index, {:locale => 'en'}, nil, {:contact => {"name"=>"Vasia"}}
+    end
+    
+    it "renders index template" do
+      get :index, :locale => 'en'
+      response.should render_template(:index)
+    end
+  end
+  
+  describe "POST create" do
+    let(:contact_message){mock_model(ContactMessage, :save => true).as_new_record}
+    before :each do
+      ContactMessage.stub(:new).and_return contact_message
+    end
+    
+    it "creates new contact message" do
+      ContactMessage.should_receive(:new).with "name"=>"Vasia"
+      post :create, :locale => 'en', :contact => {"name"=>"Vasia"}
+    end
+    
+    it "saves the contact message" do
+      contact_message.should_receive :save
+      post :create, :locale => 'en'
+    end
+    
+    it "redirects to index" do
+      post :create, :locale => 'en'
+      response.should redirect_to(contacts_path)
+    end
+    
+    it "sets flash[:notice] when saving succesfull" do
+      post :create, :locale => 'en'
+      flash[:notice].should=~ /.+/
+    end
+    
+    context "when saving failed" do
+      before :each do
+        contact_message.stub(:save).and_return false
+      end
+      
+      it "sets flash[:alert]" do
+        post :create, :locale => 'en'
+        flash[:alert].should=~ /.+/
+      end
+      
+      it "sets flash[:category] with params[:category]" do
+        post :create, :locale => 'en', :contact => {"name"=>"Vasia"}
+        flash[:contact].should == {"name"=>"Vasia"}
+      end
     end
   end
 end
