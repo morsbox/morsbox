8743b63b4c9beb496e3253d33973596ebb28eab4
Mik_Die <MikDiet@gmail.com>
2011-06-13 00:21:00 +0800

    added dialogs for description's forms

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,18 @@
 $(function() {
+  // section filter
   $('#section_filter_form #section_id').change(function(){
     $('#section_filter_form').submit()
+  });
+  
+  // description's form dialogs
+  $('.dialog_description').dialog({
+    autoOpen: false,
+    modal: true,
+    height: 700,
+    width: 1000
+  });
+  $('a.dialog_open').click(function(){
+    $('#'+$(this).attr('dialog_id')).dialog('open');
+    return false;
   });
 });
