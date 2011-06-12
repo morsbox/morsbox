3a369219726dde7230c5da5dccd763af7a55aa54
Mik_Die <MikDiet@gmail.com>
2011-06-12 22:23:52 +0800

    removed pending specs

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,6 +2,10 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.enabled.find params[:id]
     @section = @project.section
-    @projects = @section.projects.enabled
+    if @section.enabled
+      @projects = @section.projects.enabled
+    else
+      render_404
+    end
   end
 end
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -2,32 +2,39 @@ require 'spec_helper'
 
 describe ProjectsController do
   describe "GET show" do
-    let(:section2){mock_model Section, :name => "Section 2"}
+    let(:section2){mock_model Section, :name => "Section 2", :enabled => true}
     let(:project1){mock_model Project, :name => "Project 1"}
     let(:project2){mock_model Project, :name => "Project 2", :id => 2}
     before :each do
       Project.stub_chain(:enabled, :find).with(2).and_return project2
       project2.stub(:section).and_return section2
       section2.stub_chain(:projects, :enabled).and_return [project1, project2]
-      get :show, :id => 2, :locale => 'en'
     end
     
     it "assigns @project" do
+      get :show, :id => 2, :locale => 'en'
       assigns[:project].should == project2
     end
     
     it "assigns @section" do
+      get :show, :id => 2, :locale => 'en'
       assigns[:section].should == section2
     end
     
     it "assigns @projects" do
+      get :show, :id => 2, :locale => 'en'
       assigns[:projects].should == [project1, project2]
     end
     
     it "renders show view" do
+      get :show, :id => 2, :locale => 'en'
       response.should render_template(:show)
     end
     
-    it "raise error if project's section is disabled"
+    it "renders 404 if project's section is disabled" do
+      section2.stub(:enabled).and_return false
+      get :show, :id => 2, :locale => 'en'
+      response.should render_template("pages/404")
+    end
   end
 end
