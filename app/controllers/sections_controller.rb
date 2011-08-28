d742049ac2e582952b1972c98309d754f1f18a6e
Mikhail Dieterle <MikDiet@gmail.com>
2011-08-28 17:24:51 +0800

    selected section shows first work in this section

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -2,5 +2,7 @@ class SectionsController < ApplicationController
   def show
     @section = Section.enabled.find params[:id]
     @projects = @section.projects.enabled
+    @project = @projects.first
+    render "projects/show"
   end
 end
